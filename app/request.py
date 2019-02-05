df4b898c6eddbf1d988762c4e6992958baf78976
James Mwangi <james_mwangi@aol.com>
2019-02-05 09:39:55 +0300

    update

--- app/request.py
+++ app/request.py
@@ -1,10 +1,11 @@
 from app import app
 import urllib.request,json
-from .models import Sources
+from app import models
+Source =source.Source
 
-api_key = None
-base_url = None
-source_url = None
+api_key = app.config['NEWS_API_KEY']
+base_url = app.config['BASE_NEWS_API_URL']
+sources_url = app.config['SOURCE_NEWS_URL']
 
 def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from ..request import get_sources
+from .request import get_sources
 
 # Views
 @app.route('/')
@@ -11,7 +11,7 @@ def index():
     '''
     title = "NewsHub."
     news_sources = get_sources('general')
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title,news_sources = news_sources)
 
 @app.route('/news/<source_id>')
 def news(source_id):
@@ -19,5 +19,5 @@ def news(source_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
-
+    news = get_articles(id)
     return render_template('news.html', news = news)
